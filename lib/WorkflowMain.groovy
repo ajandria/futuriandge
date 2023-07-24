11f280402b4581f13d153e0e775c1ec130324347
SodzieR <xsodziero@gmail.com>
2023-07-24 16:25:19 +0200

    fix profiles

--- lib/WorkflowMain.groovy
+++ lib/WorkflowMain.groovy
@@ -8,14 +8,16 @@ class WorkflowMain {
     // Citation string for pipeline
     //
     public static String citation(workflow) {
-        return "If you use ${workflow.manifest.name} for your analysis please cite:\n\n" +
+        return "Processes:"// "If you use ${workflow.manifest.name} for your analysis please cite:\n\n" +
             // TODO nf-core: Add Zenodo DOI for pipeline after first release
             //"* The pipeline\n" +
             //"  https://doi.org/10.5281/zenodo.XXXXXXX\n\n" +
-            "* The nf-core framework\n" +
-            "  https://doi.org/10.1038/s41587-020-0439-x\n\n" +
-            "* Software dependencies\n" +
-            "  https://github.com/${workflow.manifest.name}/blob/master/CITATIONS.md"
+            //"* The nf-core framework\n" +
+            //"  https://doi.org/10.1038/s41587-020-0439-x\n\n" +
+            //"* Software dependencies\n" +
+            //"  https://github.com/${workflow.manifest.name}/blob/master/CITATIONS.md"
+            //"* Ajandria repo (author)\n" +
+            //"  https://github.com/ajandria/futuriandge"
     }
 
     //
