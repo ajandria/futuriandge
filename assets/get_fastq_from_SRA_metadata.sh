0157094540b9cce8972cc8b81beee4b63b9d0a03
Adrian <adrian.janucik@gmail.com>
2023-10-12 11:40:16 +0200

    fix proper GSM parsing

--- assets/get_fastq_from_SRA_metadata.sh
+++ assets/get_fastq_from_SRA_metadata.sh
@@ -1,30 +1,43 @@
 #!/bin/bash
 
-# Define the input CSV file from SRA
-input_file="metadata.txt"
+# Set path to sra-tools binaries
+export sra_tools_path=/Users/adrian/sratoolkit.3.0.7-mac64/bin
+
+# Define the input CSV file
+input_file=$1
 
 # Read each line from the CSV file, skipping the header
 tail -n +2 "$input_file" | while IFS=, read -r run etc
 do
     # Extract the GSM sample name from the line
-    gsm_sample_name=$(echo "$etc" | cut -d',' -f17)
+    # Step 1: Remove everything before GSM
+    gsm_temp=$(echo "$etc" | sed 's/.*GSM/GSM/')
+    # Step 2: Remove everything after the comma, including the comma
+    gsm_id=$(echo "$gsm_temp" | sed 's/,.*//')
+
+    echo "SRA id: ${run}"
+    echo "GSM id: ${gsm_id}"
 
     # Run fasterq-dump for the current SRA ID
-    fasterq-dump -v -p -x -O "$run" "$run"
+    ${sra_tools_path}/fasterq-dump -v -p -x -O "$run" "$run"
     
     # Check if the first fastq file exists
     if [[ -f "$run/${run}_1.fastq" ]]; then
         # Create an empty GSM fastq file for R1 if it doesn't exist
-        [[ ! -f "${gsm_sample_name}_R1.fastq" ]] && touch "${gsm_sample_name}_R1.fastq"
+        [[ ! -f "${gsm_id}_R1.fastq" ]] && touch "${gsm_id}_R1.fastq"
         # Append the content of the SRA fastq file to the GSM fastq file for R1
-        cat "$run/${run}_1.fastq" >> "${gsm_sample_name}_R1.fastq"
+        echo "[Dump Log] Dumping ${run}_1.fastq into ${gsm_id}_R1.fastq..."
+        cat "$run/${run}_1.fastq" >> "${gsm_id}_R1.fastq"
     fi
 
     # Check if the second fastq file exists
     if [[ -f "$run/${run}_2.fastq" ]]; then
         # Create an empty GSM fastq file for R2 if it doesn't exist
-        [[ ! -f "${gsm_sample_name}_R2.fastq" ]] && touch "${gsm_sample_name}_R2.fastq"
+        [[ ! -f "${gsm_id}_R2.fastq" ]] && touch "${gsm_id}_R2.fastq"
         # Append the content of the SRA fastq file to the GSM fastq file for R2
-        cat "$run/${run}_2.fastq" >> "${gsm_sample_name}_R2.fastq"
+        echo "[Dump Log] Dumping ${run}_2.fastq into ${gsm_id}_R2.fastq..."
+        cat "$run/${run}_2.fastq" >> "${gsm_id}_R2.fastq"
     fi
 done
+
+echo "Script finished for ${input_file}"
